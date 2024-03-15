e0aec6e65a9267d525ca734d107138872c88be5d
JayAgra <69493224+JayAgra@users.noreply.github.com>
2024-03-15 11:45:55 -0700

    Create Swift project

---
+++ disease-spread/disease-spread/disease_spreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  disease_spreadApp.swift
+//  disease-spread
+//
+//  Created by Jayen Agrawal on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct disease_spreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
